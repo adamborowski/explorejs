496399ee18de84a8563ad677bb99d599b7350a9a
Adam Borowski <fl.borovsky@gmail.com>
2017-03-04 21:01:39 +0100

    [slingshoot] added background behind dialog

--- packages/explorejs-tester/src/view/dialog/dialog.js
+++ packages/explorejs-tester/src/view/dialog/dialog.js
@@ -8,34 +8,35 @@ export const Dialog = (props) => {
     return null;
   }
   const lastMessage = props.messages[0];
-  return <div key={lastMessage.id} className="modal" tabIndex="-1" role="dialog" style={{display: 'block'}}>
-    <div className="modal-dialog" role="document">
-      <div className="modal-content">
-        <div className="modal-header">
-          <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span
-            aria-hidden="true">&times;</span></button>
-          <h4 className="modal-title">Modal title</h4>
-        </div>
-        <div className="modal-body">
-          <p>{lastMessage.message}</p>
-        </div>
-        <div className="modal-footer">
-          {
-            lastMessage.resolutions.map(resolution =>
-              <button key={resolution.key}
-                      type="button"
-                      onClick={() => props.callAction(lastMessage, resolution)}
-                      className={`btn btn-default ${resolution.primary ? 'btn-primary' : ''}`}
-                      data-dismiss="modal">{resolution.message}
-              </button>
-            )
-          }
+  return <div>
+    <div key={lastMessage.id} className="modal" tabIndex="-1" role="dialog" style={{display: 'block'}}>
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span
+              aria-hidden="true">&times;</span></button>
+            <h4 className="modal-title">Modal title</h4>
+          </div>
+          <div className="modal-body">
+            <p>{lastMessage.message}</p>
+          </div>
+          <div className="modal-footer">
+            {
+              lastMessage.resolutions.map(resolution =>
+                <button key={resolution.key}
+                        type="button"
+                        onClick={() => props.callAction(lastMessage, resolution)}
+                        className={`btn btn-default ${resolution.primary ? 'btn-primary' : ''}`}
+                        data-dismiss="modal">{resolution.message}
+                </button>
+              )
+            }
+          </div>
         </div>
       </div>
     </div>
+    <div className="modal-backdrop fade in"></div>
   </div>
-
-
 };
 
 const mapStateToProps = (state) => ({
