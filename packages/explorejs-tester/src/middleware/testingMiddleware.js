36ffdcf458b6c3ea8e4012b9909ebc79da4051dd
Adam Borowski <fl.borovsky@gmail.com>
2017-03-17 20:41:19 +0100

    [slingshoot] fixed stars bug

--- packages/explorejs-tester/src/middleware/testingMiddleware.js
+++ packages/explorejs-tester/src/middleware/testingMiddleware.js
@@ -5,45 +5,59 @@ import {SESSION_SCORE, SESSION_CREATE} from "../constants/actionTypes";
 import {push} from 'react-router-redux';
 
 export default  store => next => action => {
+  const newAction = {...action};
   const state = store.getState();
-  switch (action.type) {
-    case SESSION_SCORE:
-      if (action.override == false) {
 
-        const session = createSession(state);
-        const maxAvailable = availableScoreSelector(state);
-        const currentSession = session.Session.withId(action.sessionId);
-        const currentSessionScore = currentSession.score;
-        const currentScenario = currentSession.scenario;
+  const fixAction = (score) => {
+    newAction.score = score;
+
+    store.dispatch(pushNotification(`You don't have enough stars to assign. Scored with ${newAction.score} points.`));
+  };
+
+
+  switch (newAction.type) {
+    case SESSION_SCORE:
+      const session = createSession(state);
+      const maxAvailable = availableScoreSelector(state);
+      const currentSession = session.Session.withId(newAction.sessionId);
+      const currentSessionScore = currentSession.score;
+      const currentScenario = currentSession.scenario;
+      if (newAction.override == false) {
+
         const otherScoredSessions = currentScenario.sessions.all().toModelArray().filter(a => a.id !== currentSession.id && a.score > 0);
 
         if (otherScoredSessions.length === 0) {
-          if (action.score - currentSessionScore > maxAvailable) {
-
-            action.score = maxAvailable + currentSessionScore;
-
-            store.dispatch(pushNotification(`You don't have enough stars to assign. Scored with ${action.score} points.`));
+          if (newAction.score - currentSessionScore > maxAvailable) {
+            fixAction(maxAvailable + currentSessionScore);
           }
         }
         else {
           next(operations.showDialog('There is already scored session, only one can be scored at a time', [
-            {key: 'overwrite', message: 'Overwrite', action: {...action, override: true}, primary: true},
+            {key: 'overwrite', message: 'Overwrite', action: {...newAction, override: true}, primary: true},
             {key: 'discard', message: 'Discard', action: pushNotification(`You didn't score this session.`, 'info')},
           ]));
           return;
         }
         break;
       }
+      else {
+
+        const scenarioTotalScore = currentScenario.sessions.all().toRefArray().reduce((res, s) => res + s.score, 0);
+        if (newAction.score - scenarioTotalScore > maxAvailable) {
+
+          fixAction(maxAvailable + scenarioTotalScore);
+        }
+      }
   }
 
-  next(action);
+  next(newAction);
 
-  switch (action.type) {
+  switch (newAction.type) {
     case SESSION_CREATE:
       const newItem = state.orm.Session.meta.maxId + 1;
       const session = createSession(state);
 
-      let action2 = push(`/scenario/${action.payload.scenario}/session/${newItem}`);
+      let action2 = push(`/scenario/${newAction.payload.scenario}/session/${newItem}`);
       store.dispatch(action2);
       break;
   }
