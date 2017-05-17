272030ef7676c11f831c4793634949dfa7e8aead
Adam Borowski <Adam.Borowski@consult.nordea.com>
2017-05-17 21:21:20 +0200

    part of fixing npm start in explorejs-tester

--- packages/explorejs-tester/webpack.config.dev.js
+++ packages/explorejs-tester/webpack.config.dev.js
@@ -40,7 +40,7 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.jsx?$/, include: /src/, loaders: ['babel']},
       {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file'},
       {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
       {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
