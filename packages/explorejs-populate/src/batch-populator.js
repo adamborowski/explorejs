cb9d9917294d716ed80ba79ff535505636e8557e
adam <aborowski@atlassian.com>
2017-06-17 13:30:06 +0200

    introduced producer-result pipeline

--- packages/explorejs-populate/src/batch-populator.js
+++ packages/explorejs-populate/src/batch-populator.js
@@ -5,18 +5,21 @@ const batcher = require('./utils/batcher');
 const aggregator = require('./data-aggregator');
 const Pipeline = require('./utils/producer-result-pipeline');
 
-module.exports = async (db, measurementId, levels, start, end, interval, batchSize) => {
-
-    let cnt = 0;
+const progress = (start, end, interval) => {
     const totalValues = (end - start) / interval;
-    const percent = (items) => {
+    let cnt = 0;
+
+    return (items) => {
         cnt += items.length;
         console.log(`Processsed ${cnt} of ${totalValues} (${Math.round(cnt / totalValues * 100)}%)`);
     };
+};
+
+module.exports = async (db, measurementId, levels, start, end, interval, batchSize) => {
 
     const pipeline = Pipeline(10);
     const generator = dataReader(start, end, interval);
-    const batchedProgressInfo = batcher(batchSize, percent);
+    const batchedProgressInfo = batcher(batchSize, progress(start, end, interval));
     const rawBatchedUpload = batcher(batchSize, pipeline.wrapResultFunction(batchedInsert.raw(measurementId, db)));
     const aggregationsBatchedUpload = levels
         .filter(a => a.id !== 'raw')
