91b919e406b4e14cb560ef38ba0ebb7394bc4d77
adam <aborowski@atlassian.com>
2017-07-04 02:01:40 +0200

    #19 introduce translation file for pl language

---
+++ packages/explorejs-tester/src/translations/pl_PL.js
@@ -0,0 +1,18 @@
+export default {
+  general: {
+    configuration: 'Konfiguracja',
+    testConfigurations: 'Konfiguracje testowe',
+    interactiveSurvey: 'ExploreJS interaktywna ankieta'
+  },
+  session: {
+    finish: 'Zakończ i oceń',
+    simulate: 'Symuluj wolne połączenie',
+    speed: 'Prędkość połączenia'
+  },
+  scenario: {
+    back: 'Poprzednie',
+    again: 'Jeszcze raz',
+    next: 'Następne',
+    pleasePutScore: 'Proszę ocenić tę konfigurację'
+  }
+};
