273390c552a218b9c8b5fae4fabb0a6ec45c8c5a
adam <aborowski@atlassian.com>
2017-07-05 22:42:13 +0200

    #19 translated scenario index page

--- packages/explorejs-tester/src/components/pages/scenario/ScenariosIndexPage.js
+++ packages/explorejs-tester/src/components/pages/scenario/ScenariosIndexPage.js
@@ -2,10 +2,11 @@ import  React from 'react';
 import {connect} from 'react-redux';
 import {scenarioSelector} from '../../../selectors/testingSelectors';
 import {Link} from 'react-router';
-export const ScenarioListPage = (props) => {
+import trans from '../../../translations/trans';
+export const ScenarioListPage = trans()((props, {trans, dynamicTrans}) => {
   return (
     <div>
-      <h1 className="page-header">Testing scenarios</h1>
+      <h1 className="page-header">{trans('general.testConfigurations')}</h1>
       <p>
         TODO: show diagram of general problem - server aggregations, how much data we want do visualize, choosing proper level
       </p>
@@ -14,7 +15,7 @@ export const ScenarioListPage = (props) => {
           props.scenarios.map((scenario) =>
             (<Link key={scenario.id} href="#" className="list-group-item" to={`/scenario/${scenario.id}`}>
               <span className="badge">{scenario.sessions.length}</span>
-              <h4 className="list-group-item-heading">{scenario.name}</h4>
+              <h4 className="list-group-item-heading">{dynamicTrans(scenario.name)}</h4>
             </Link>)
           )
         }
@@ -23,7 +24,7 @@ export const ScenarioListPage = (props) => {
 
     </div>
   );
-};
+});
 
 ScenarioListPage.propTypes = {
   scenarios: React.PropTypes.array
