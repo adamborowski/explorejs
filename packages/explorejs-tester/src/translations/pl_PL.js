28fcf6f7cd92cb2ea2edf752a71defc335e70650
adam <aborowski@atlassian.com>
2017-08-13 10:08:48 +0200

    #19 fix5 typo

--- packages/explorejs-tester/src/translations/pl_PL.js
+++ packages/explorejs-tester/src/translations/pl_PL.js
@@ -64,7 +64,7 @@ export default {
         question: 'Jak podoba Ci sie ankieta?',
         answers: ['Znudziłem się', 'Jest męcząca', 'Ciężko stwierdzić', 'Nieźle', 'Zaciekawiła mnie']
       },
-      {question: 'Możesz krótko opisać, jakie zagadnie zostało tutaj poruszone?'},
+      {question: 'Możesz krótko opisać, jakie zagadnienie zostało tutaj poruszone?'},
       {question: 'Możesz krótko opisać, co jaki problem jest tu adresowany?'},
       {
         question: 'Czy uważasz, że problem w ogóle istnieje?',
