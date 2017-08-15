442af6c405464a69d911f2ed1d2c0684e942ad9f
adam <aborowski@atlassian.com>
2017-08-16 00:27:34 +0200

    #27 aded tabs

--- packages/explorejs-tester/src/features/results/components/ResultDetailView.js
+++ packages/explorejs-tester/src/features/results/components/ResultDetailView.js
@@ -7,6 +7,7 @@ import {scenarioSelector} from '../../../selectors/testingSelectors';
 import {compose} from 'redux';
 import ScoresView from './ScoresView';
 import QuestionsView from './QuestionsView';
+import {Tab, Tabs} from 'react-bootstrap';
 
 
 class ResultDetailView extends React.Component {
@@ -33,20 +34,22 @@ class ResultDetailView extends React.Component {
 
     return (
       <div className="results-detail-view">
-        <div className="page-header">
-          <h3>Response from {result.name || <span className="result-no-name">unknown person</span>}
-            <small> {formatDate(result.time)}</small>
-          </h3>
-        </div>
+        <h3>Response from {result.name || <span className="result-no-name">unknown person</span>}
+          <small> {formatDate(result.time)}</small>
+        </h3>
         <div>
-
-          <h5 className="page-header">Scores</h5>
-          <ScoresView scores={scores} possibleAnswers={possibleAnswers}/>
-          <h5 className="page-header">Questions</h5>
-          <QuestionsView
-            questions={result.data.questions}
-            answers={result.data.answers}
-          />
+          <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
+            <Tab eventKey={1} title="Scores">
+              <ScoresView scores={scores} possibleAnswers={possibleAnswers}/>
+            </Tab>
+            <Tab eventKey={2} title="Summary">
+              <QuestionsView
+                questions={result.data.questions}
+                answers={result.data.answers}
+              />
+            </Tab>
+            <Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</Tab>
+          </Tabs>
         </div>
       </div>
     );
